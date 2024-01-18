45fe72be0f5a6ddf5b9dcdb70a83b90d1f3babfe
MihaelProdanovic <154518732+MihaelProdanovic@users.noreply.github.com>
2024-01-18 11:27:16 +0100

    Add files via upload

---
+++ 1. zadatak.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace InicijalnoPonavljanje
+{
+    class Program
+    {
+        static void Main()
+        {
+            
+            int a = 100;
+            int b = 3;
+
+           
+            int quotient = a / b;
+            int remainder = a % b;
+
+            
+            Console.WriteLine($"\"a dijeljeno s b je {quotient} i ostatak {remainder}\"");
+
+            
+            Console.ReadLine();
+        }
+    }
+}
+
---
+++ 2. zadatak.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp5
+{
+    class Program
+    {
+        static void Main()
+        {
+            // Zatraži unos niza znakova od korisnika
+            Console.WriteLine("Unesite niz znakova:");
+            string unos = Console.ReadLine();
+
+            
+            string rezultat = ZamijeniRazmake(unos);
+
+           
+            Console.WriteLine("Rezultat zamjene razmaka:");
+            Console.WriteLine(rezultat);
+
+           
+            Console.ReadLine();
+        }
+
+        static string ZamijeniRazmake(string originalniNiz)
+        {
+            
+            return originalniNiz.Replace(' ', '_');
+        }
+    }
+}
