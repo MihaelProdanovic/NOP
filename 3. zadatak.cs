a2ce8cd6fbafcf312788c8f771f4b7a9889e4d5a
MihaelProdanovic <154518732+MihaelProdanovic@users.noreply.github.com>
2024-01-18 13:36:44 +0100

    Add files via upload

---
+++ 3. zadatak.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp6
+{
+    class Program
+    {
+        static void Main()
+        {
+           
+            int a, b;
+            double c, d;
+
+         
+            a = 10;
+            b = 7;
+            c = 12.5;
+            d = 16.7;
+
+            
+            double prosjek = (a + b + c + d) / 4.0;
+
+           
+            Console.WriteLine("Prosjek vrijednosti je: {0}", prosjek);
+
+            
+            Console.ReadLine();
+        }
+    }
+}
