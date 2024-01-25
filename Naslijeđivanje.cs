ca6f7342bb741193cc43a440fcafdb0f51699ed8
MihaelProdanovic <154518732+MihaelProdanovic@users.noreply.github.com>
2024-01-25 11:05:04 +0100

    Add files via upload

---
+++ Naslijeđivanje.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Naslijeđivanje
+{
+   class Cat
+    {
+        bool opasna;
+
+        public bool Opasna { get => opasna; set => opasna = value; }
+
+        void Opasnost()
+        {
+            Opasna = false;
+        }
+    }
+
+    class Gepard : Cat
+    {
+        public override void Opasnost()
+        {
+            Opasna = true;
+        }
+    }
+
+}
