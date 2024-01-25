46ec0b29446d9e096505486639ca9fb32b3453be
MihaelProdanovic <154518732+MihaelProdanovic@users.noreply.github.com>
2024-01-25 11:23:51 +0100

    Add files via upload

---
+++ Naslijeđivanje vjezba 1.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Naslijeđivanje_vjezba_1
+{
+
+    class NebeskoTijelo
+    {
+        protected double BrzinaOkoOsi;
+        protected double BrzibaOkoSunca;
+    }
+
+    class Planet : NebeskoTijelo
+    {
+        public Planet(double d1, double d2)
+        {
+            BrzinaOkoOsi = d1;
+            BrzibaOkoSunca = d2;
+        }
+    }
+
+    class Satelit : NebeskoTijelo
+    {
+        public Satelit(double d1, double d2)
+        {
+            BrzinaOkoOsi = d1;
+            BrzibaOkoSunca= d2;
+        }
+    }
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Planet Zemlja = new Planet(1000, 2000);
+            Satelit Mjesec = new Satelit(100, 200);
+        }
+    }
+}
