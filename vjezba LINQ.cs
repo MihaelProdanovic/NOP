4cedba898ec1ced8b093c42c7d7073842c1d60e5
MihaelProdanovic <154518732+MihaelProdanovic@users.noreply.github.com>
2024-03-14 10:23:24 +0100

    Add files via upload

---
+++ vjezba LINQ.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp22
+{
+    internal class Program
+    {
+        public static void Main()
+        {
+            // Student collection
+            List<Student> studentList = new List<Student>() {
+                new Student() { StudentID = 1, StudentName = "John" } ,
+                new Student() { StudentID = 2, StudentName = "Moin" } ,
+                new Student() { StudentID = 3, StudentName = "Bill" } ,
+                new Student() { StudentID = 4, StudentName = "Ram" } ,
+                new Student() { StudentID = 5, StudentName = "Ron"  }
+            };
+
+            // upit i izvršavanje upita
+            var rezultat = from student in studentList
+                           where student.StudentName.Contains('o')
+                           select student.StudentName;
+
+            foreach (var rez in rezultat)
+            {
+                Console.WriteLine(rez);
+            }
+            Console.ReadLine();
+        }
+    }
+
+
+    public class Student
+    {
+
+        public int StudentID { get; set; }
+        public string StudentName { get; set; }
+    }
+
+}
+
