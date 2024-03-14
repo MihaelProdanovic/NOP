494689fac464c7f95e0113d981fcb5e8f54ff481
MihaelProdanovic <154518732+MihaelProdanovic@users.noreply.github.com>
2024-03-14 08:46:22 +0100

    Add files via upload

---
+++ Geometrijska_tijela.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ponavljanjezaispit
+{
+    public class GeometrijskoTijelo
+    {
+        public double Volumen;
+        public double Oplosje;
+        public static int BrojacTijela;
+
+        public GeometrijskoTijelo()
+        {
+            BrojacTijela++;
+        }
+    }
+
+    public class Kugla : GeometrijskoTijelo
+    {
+        // Dodatne varijable ili metode specifične za kuglu
+    }
+
+    public class Kvadar : GeometrijskoTijelo
+    {
+        // Dodatne varijable ili metode specifične za kvadar
+    }
+
+    public class Kocka : Kvadar
+    {
+        // Dodatne varijable ili metode specifične za kocku
+    }
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Kugla x = new Kugla();
+            Kvadar y = new Kvadar();
+            Kocka z = new Kocka();
+
+            Console.WriteLine("Broj geometrijskih tijela: " + GeometrijskoTijelo.BrojacTijela);
+
+            Console.ReadKey();
+        }
+    }
+}
